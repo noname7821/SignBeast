b986e8157cd902cce22cbfce9c9564c733e34fea
mintoo <kaanistgut46@gmail.com>
2026-09-04 20:34:09 +0200

    Add files via upload

--- build_zsign.sh
+++ build_zsign.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd /opt/render/project/src
+
 rm -rf zsign
 git clone https://github.com/zhlynn/zsign.git
 cd zsign/build/linux
 make
 cp ../../bin/zsign /opt/render/project/src/zsign
-chmod +x /opt/render/project/src/zsign
+
 chmod 755 /opt/render/project/src/zsign
+ls -la /opt/render/project/src/zsign
