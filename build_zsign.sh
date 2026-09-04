45a27e7ff9ed0939ef6577b06b1d2b671609446e
mintoo <kaanistgut46@gmail.com>
2026-09-04 22:45:29 +0200

    Add files via upload

--- build_zsign.sh
+++ build_zsign.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /opt/render/project/src
 
-apt-get update
-apt-get install -y build-essential libssl-dev pkg-config git
+apt-get update > /dev/null 2>&1
+apt-get install -y build-essential libssl-dev pkg-config git > /dev/null 2>&1
 
-rm -rf zsign
-git clone https://github.com/zhlynn/zsign.git
-cd zsign/build/linux
+rm -rf zsign_src
+git clone https://github.com/zhlynn/zsign.git zsign_src > /dev/null 2>&1
+cd zsign_src/build/linux
 
-make clean
-make
+make clean > /dev/null 2>&1
+make > /dev/null 2>&1
 
 cp ../../bin/zsign /opt/render/project/src/zsign
 chmod 755 /opt/render/project/src/zsign
